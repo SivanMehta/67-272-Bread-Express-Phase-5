c3a07964183c1fcf6a543801f5464da5c1b988af
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-04-25 02:00:42 -0400

    generalized redirection back to user profile during interactions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
-  def get_address_options(user=nil)
-    if user.nil? || user.role?(:admin)
-      addresses = Address.active.by_recipient.to_a
-    else
-      addresses = user.customer.addresses.by_recipient.to_a
+    def get_address_options(user=nil)
+        if user.nil? || user.role?(:admin)
+            addresses = Address.active.by_recipient.to_a
+        else
+            addresses = user.customer.addresses.by_recipient.to_a
+        end
+          addresses.map{|addr| ["#{addr.recipient} : #{addr.street_1}", addr.id] }
     end
-    addresses.map{|addr| ["#{addr.recipient} : #{addr.street_1}", addr.id] }
-  end
+
+    def get_user_profile
+        if current_user.role? :customer
+            return customer_path(Customer.find_by_user_id(current_user.id))
+        else
+            return user_path(current_user.id)
+        end
+    end
 end
