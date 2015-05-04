7d6d3cf554ca23f033a606b4bae35d4ca8e0c574
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-05-03 23:20:11 -0400

    adding shipping list to homepage for admins and shippers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
     include BreadExpressHelpers::Baking
 
     def home
+        if !current_user.nil? and (current_user.role? :admin or current_user.role? :shipper)
+            @pending_orders = Order.not_shipped.chronological.paginate(:page => params[:page]).per_page(15)
+        end
     end
 
     def about
